27c698b68310facb46ed3e628adf57d7dd559929
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-12-18 09:23:05 -0500

    added more to the app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-
 import { Provider } from 'react-redux';
 import store from '../store';
 
