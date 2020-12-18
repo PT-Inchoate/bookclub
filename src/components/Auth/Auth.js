05d32ffffb4b5f4c750d76a900c854a87cc01328
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-12-18 15:18:55 -0500

    added vslidation for auth input

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -10,25 +10,75 @@ export class Auth extends Component {
         username: '',
         email: '',
         password: '',
-        errors: [],
+        errors: {},
         loading: false,
         prevLogin: true
     }
+
+    isFormValid = ({ email, password }) => email && password;
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value })
     }
 
+    //checking values that were set on state from login form
+    handleInputErrors = values => {
+        // Email Errors
+        if (!values.email) {
+            this.setState(prevState => ({
+                errors: {                   
+                    email: 'Email required'      
+                }
+            }))
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            this.setState(prevState => ({
+                errors: {                   
+                    email: 'Invalid email address'      
+                }
+            }))
+        }
+        // Password Errors
+        if (!values.password) {
+            this.setState(prevState => ({
+                errors: {                   
+                    password: 'Password required'   
+                }
+            }))
+        } else if (values.password.length < 6) {
+            this.setState(prevState => ({
+                errors: {
+                    ...prevState.errors.password,                    
+                    password: 'Password must be at least 6 characters'      
+                }
+            }))
+        }
+        else {
+            this.setState(prevState => ({
+                errors: {
+                    ...prevState.errors.password,                    
+                    password: ''      
+                }
+            }))
+        }
+      }
+
     handleSubmit = event => {
         event.preventDefault();
+        this.handleInputErrors(this.state);
+        console.log('this.setState ', this.state)
+
+        if(this.isFormValid(this.state)) {
+            this.setState({ errors: [], loading: true });
+            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);   
+        }
     }
 
     render() {
-        console.log('props.match.path ', this.props.match.path)
-        const { email, password, username, prevLogin } = this.state;
+        const { email, password, username, prevLogin, errors } = this.state;
 
         const isLogin = this.props.match.path === '/login';
         const pageTitle = isLogin ? 'Login' : 'Sign Up';
+        const linkTitle = isLogin ? 'Sign Up' : 'Login';
         const descriptionLink = isLogin ? '/signup': '/login';
         const descriptionText = isLogin ? 'Don’t have an account?' : 'Have an account?';
 
@@ -65,6 +115,8 @@ export class Auth extends Component {
                         autoComplete="off"
                     /> 
 
+                    { errors.email && <p className="Auth-error">{ errors.email }</p>}
+
                     <input 
                         className="Auth-form__input"
                         onChange={this.handleChange}
@@ -73,6 +125,8 @@ export class Auth extends Component {
                         type="password"
                         placeholder="Password"
                     />
+
+                    { errors.password && <p className="Auth-error">{ errors.password }</p>}
 
                     <div className="Auth-checkbox">
                         {pageTitle === 'Login' && (
@@ -92,7 +146,7 @@ export class Auth extends Component {
                     <Link to={descriptionLink} 
                         className="color-grey-light"
                         onClick={() => (prevLogin => !prevLogin)}
-                    >{descriptionText}</Link>
+                    >{descriptionText}<span>{linkTitle}</span></Link>
                 </div>
                 
             </div>
