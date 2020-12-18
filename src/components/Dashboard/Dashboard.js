8c4b11dc17f4b65cba849849094aa0318ecaa2fe
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-12-17 20:14:15 -0500

    set up enzyme

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+
+function Dashboard() {
+  return (
+    <div className="App">
+      <div className="App-Sidebar">Sidebar</div>
+      <div className="App-Dashboard">Dashboard
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <button className="button-primary">learn more</button>
+        <button className="button-primary__big">learn more</button>
+        
+        <p style={{color: "rgb(244,89,89)"}}>
+          Bookclub
+        </p>
+      </header>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
